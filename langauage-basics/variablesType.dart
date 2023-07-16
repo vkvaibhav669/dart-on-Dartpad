5917b2510ab56d7a0f7096d6ac78402ce2cc2a11
vkvaibhavtb <vaibhavkumar_rajkumar@srmuniv.edu.in>
2023-07-16 21:11:38 +0530

    Create variablesType.dart

---
+++ langauage-basics/variablesType.dart
@@ -0,0 +1,11 @@
+void main() {
+  print('Hello world');
+  String name = "Hello world";
+
+  int age = 25;
+  double data = 25.55;
+  bool knowDart = false;
+  print('age is $age');
+  print('Double data is $data');
+  print('Do i know dart is $knowDart');
+}
